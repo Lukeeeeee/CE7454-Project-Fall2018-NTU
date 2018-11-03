525cb053144cef572b75e44315337f3082f3db9d
Lukeeeeee <lukedong123@gmail.com>
2018-11-03 11:26:47 +0800

    re-format codes, working on fixing bugs of produce image

--- src/train.py
+++ src/train.py
@@ -18,6 +18,7 @@ from utils.config import Config
 from src.model import ICNet_BN
 from utils.config import Config
 from utils.image_reader import ImageReader, prepare_label
+from src.util import get_mask, create_bce_loss, create_losses, global_logits, global_label
 
 
 def get_arguments():
@@ -40,75 +41,6 @@ def get_arguments():
     return parser.parse_args()
 
 
-def get_mask(gt, num_classes, ignore_label):
-    less_equal_class = tf.less_equal(gt, num_classes - 1)
-    not_equal_ignore = tf.not_equal(gt, ignore_label)
-    mask = tf.logical_and(less_equal_class, not_equal_ignore)
-    indices = tf.squeeze(tf.where(mask), 1)
-
-    return indices
-
-def create_bce_loss(output, label, num_classes, ignore_label):
-    raw_pred = tf.reshape(output, [-1, num_classes])
-    label = prepare_label(label, tf.stack(output.get_shape()[1:3]), num_classes=num_classes, one_hot=False)
-    label = tf.reshape(label, [-1, ])
-
-    indices = get_mask(label, num_classes, ignore_label)
-    gt = tf.cast(tf.gather(label, indices), tf.int32)
-    gt_one_hot= tf.one_hot(gt, num_classes)
-    pred = tf.gather(raw_pred, indices)
-    BCE = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=pred, labels=gt_one_hot))
-
-    inse = tf.reduce_sum(pred * tf.cast(gt_one_hot,tf.float32))
-    l = tf.reduce_sum(pred)
-    r = tf.reduce_sum(tf.cast(gt_one_hot,tf.float32))
-    dice = tf.math.log((2. * inse + 1e-5) / (l + r + 1e-5))
-
-
-    loss = BCE-dice
-    reduced_loss = tf.reduce_mean(loss)
-
-    return reduced_loss
-
-global_logits = []
-global_label = []
-
-
-def create_loss(output, label, num_classes, ignore_label):
-    raw_pred = tf.reshape(output, [-1, num_classes])
-    label = prepare_label(label, tf.stack(output.get_shape()[1:3]), num_classes=num_classes, one_hot=False)
-    label = tf.reshape(label, [-1, ])
-
-    indices = get_mask(label, num_classes, ignore_label)
-    gt = tf.cast(tf.gather(label, indices), tf.int32)
-    pred = tf.gather(raw_pred, indices)
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=pred, labels=gt)
-    reduced_loss = tf.reduce_mean(loss)
-
-    global_label.append(gt)
-    global_logits.append(pred)
-
-    return reduced_loss
-
-
-def create_losses(net, label, cfg):
-    # Get output from different branches
-    sub4_out = net.layers['sub4_out']
-    sub24_out = net.layers['sub24_out']
-    sub124_out = net.layers['conv6_cls']
-
-    loss_sub4 = create_loss(sub4_out, label, cfg.param['num_classes'], cfg.param['ignore_label'])
-    loss_sub24 = create_loss(sub24_out, label, cfg.param['num_classes'], cfg.param['ignore_label'])
-    loss_sub124 = create_loss(sub124_out, label, cfg.param['num_classes'], cfg.param['ignore_label'])
-
-    l2_losses = [cfg.WEIGHT_DECAY * tf.nn.l2_loss(v) for v in tf.trainable_variables() if 'weights' in v.name]
-
-    # Calculate weighted loss of three branches, you can tune LAMBDA values to get better results.
-    reduced_loss = cfg.LAMBDA1 * loss_sub4 + cfg.LAMBDA2 * loss_sub24 + cfg.LAMBDA3 * loss_sub124 + tf.add_n(l2_losses)
-
-    return loss_sub4, loss_sub24, loss_sub124, reduced_loss
-
-
 class TrainConfig(Config):
     def __init__(self, dataset, is_training, filter_scale=1, random_scale=None, random_mirror=None, log_path_end=''):
         Config.__init__(self, dataset, is_training, filter_scale, random_scale, random_mirror,
@@ -127,7 +59,7 @@ class TrainConfig(Config):
     LEARNING_RATE = 5e-4
 
 
-def main(lr=None):
+def main(lr=None, log_path_end=''):
     """Create the model and start the training."""
     tf.reset_default_graph()
     args = get_arguments()
@@ -144,7 +76,7 @@ def main(lr=None):
                       random_scale=args.random_scale,
                       random_mirror=args.random_mirror,
                       filter_scale=args.filter_scale,
-                      log_path_end='lr=%s' % lr)
+                      log_path_end=log_path_end)
     if lr:
         cfg.LEARNING_RATE = lr
     tf.set_random_seed(cfg.RANDOM_SEED)
@@ -210,6 +142,7 @@ def main(lr=None):
                     [reduced_loss, loss_sub4, loss_sub24, loss_sub124, val_reduced_loss, train_op, \
                      train_net.labels, train_net.layers['conv6_cls'], global_label[-1], global_logits[-1]],
                     feed_dict=feed_dict)
+                # print(label)
             duration = time.time() - start_time
             log = {
                 'LOSS_VALUE': float(loss_value),
@@ -237,4 +170,4 @@ def main(lr=None):
 if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-    main()
+    main(log_path_end='_debug')
