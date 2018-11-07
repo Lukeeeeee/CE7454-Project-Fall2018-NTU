80bc5b8c25634285dd0e4e36ebc362423763e60e
erichhhhho <erichhhhho@hotmail.com>
2018-11-07 10:07:29 +0700

    fix color_mat and restore inference mode prect(feed_dict)

--- src/train.py
+++ src/train.py
@@ -137,7 +137,7 @@ class TrainConfig(Config):
 
     # Set pre-trained weights here (You can download weight using `python script/download_weights.py`) 
     # Note that you need to use "bnnomerge" version.
-    model_weight = '../model/cityscapes/icnet_cityscapes_trainval_90k_bnnomerge.npy'
+    model_weight = '../model/cityscapes/icnet_cityscapes_train_30k_bnnomerge.npy'
 
     # Set hyperparameters here, you can get much more setting in Config Class, see 'utils/config.py' for details.
     LAMBDA1 = 0.16
